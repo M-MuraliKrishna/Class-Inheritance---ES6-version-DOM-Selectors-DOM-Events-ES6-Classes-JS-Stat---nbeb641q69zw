24667de5b624ac791e80e061fa4ddaabd270d2a2
Murali Krishna M <murali24011999@gmail.com>
2023-01-03 00:00:01 +0530

    Updated

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+const Shape = require('./shape');
+const Circle = require('./circle');
+
+const circle = new Circle('red', 5);
+console.log(circle.calculateArea()); // prints 78.53981633974483
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+
+  drawShape() {
+    // implementation for drawing the shape
+  }
+
+  calculateArea() {
+    // implementation for calculating the area of the shape
+  }
+}
+
+module.exports = Shape;
